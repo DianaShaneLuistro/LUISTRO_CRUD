9889a0d4c12e459490665c357934d4d412bc0c27
DianaShaneLuistro <dianashaneluistro@gmail.com>
2025-09-15 16:13:25 +0800

    xcvb

--- app/config/database.php
+++ app/config/database.php
@@ -59,15 +59,16 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12798308',
+    'password'	=> '1D4nFNHqvK',
+    'database'	=> 'sql12798308',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
     'path'      => ''
 );
+
 
 ?>
